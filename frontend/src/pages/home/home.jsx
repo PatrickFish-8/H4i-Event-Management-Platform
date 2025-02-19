3268f3dd49e30cc58bd7210af84f42063b7cf181
Patrick Fish <patrickfish10@icloud.com>
2025-02-19 16:16:18 -0500

    navbar component

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,72 +1,13 @@
 import React from 'react';
+import NavBar from '../../components/navbar/navbar.jsx';
 import './home.css';
 
 const Home = () => {
 
-    const submit = async () => {
-        try {
-            const title = document.getElementById('title');
-            const description = document.getElementById('description');
-            const date = document.getElementById('date');
-            const time = document.getElementById('time');
-            const location = document.getElementById('location');
-
-            const jsonstring = JSON.stringify({ title: title.value, description: description.value, date: date.value, time: time.value, location: location.value });
-            console.log(jsonstring);
-
-            if (!title || !description || !date || !time || !location) return
-
-            const response = await fetch('http://localhost:3000/createEvent', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ title: title.value, description: description.value, date: date.value, time: time.value, location: location.value })
-            });
-            if (!response.ok) {
-                throw new Error('Oops, something went wrong!');
-            }
-
-            console.log("uploaded to database");
-            
-            title.value = "";
-            description.value = "";
-            date.value = "";
-            time.value = "";
-            location.value = "";
-            
-        } catch (err) {
-            console.error(err);
-        }
-        
-    }
-
     return (
-        <div>
-            <form onSubmit={(e) => e.preventDefault()}>
-                <label htmlFor='title'>Title: </label>
-                <input type='text' 
-                id='title'/>
-                <br/>
-                <label htmlFor='description'>Description: </label>
-                <input type='text' 
-                id='description'/>
-                <br/>
-                <label htmlFor='date'>Date: </label>
-                <input type='text' 
-                id='date'/>
-                <br/>
-                <label htmlFor='time'>Time: </label>
-                <input type='text' 
-                id='time'/>
-                <br/>
-                <label htmlFor='location'>Location: </label>
-                <input type='text' 
-                id='location'/>
-                <br/>
-                <button type='submit' onClick={submit}>Submit</button>
-            </form>
-        </div>
+        <>
+            <NavBar />
+        </>
     );
 };
 
