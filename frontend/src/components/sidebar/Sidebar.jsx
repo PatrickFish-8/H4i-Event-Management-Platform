797ddfe794983514c721c8e6bdd460da655ccf4d
Sadid Ethun <sethun@bu.edu>
2025-02-26 20:28:15 -0500

    changes to sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -8,6 +8,7 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
   const [predictedBudget, setPredictedBudget] = useState("");
   const [actualSpent, setActualSpent] = useState("");
   const [attendance, setAttendance] = useState("");
+  const [net, setNet] = useState(predictedBudget - actualSpent);
 
   // When the selectedEvent changes, update the state values
   useEffect(() => {
@@ -20,13 +21,13 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
 
   // calls updateEvent when the predictedBudget, actualSpent, or attendance changes
   useEffect(() => {
-    if(selectedEvent) {
+    if (selectedEvent) {
       updateEvent();
     }
   }, [predictedBudget, actualSpent, attendance]);
 
   const updateEvent = async () => {
-    try { 
+    try {
       const response = await fetch("http://localhost:3000/updateEvent", {
         method: "POST",
         headers: {
@@ -51,6 +52,12 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
     }
   }
 
+  useEffect(() => {
+    setNet(predictedBudget - actualSpent);
+  }, [predictedBudget, actualSpent]);
+
+
+
   return (
     <div className={`home_sidebarContainer ${selectedEvent ? "open" : ""}`}>
       <IconButton id="iconButton" onClick={closeSidebar}>
@@ -60,7 +67,7 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
         <div className="sidebar_content">
           <h1 className="sidebar_title">{selectedEvent.title}</h1>
           <h2 className="sidebar_subtitle">
-            {selectedEvent.date} {selectedEvent.time.start} - {selectedEvent.time.end}
+            {selectedEvent.date} {selectedEvent.time.start}-{selectedEvent.time.end}
           </h2>
           <h2 className="sidebar_subtitle">{selectedEvent.location}</h2>
           <p>{selectedEvent.description}</p>
@@ -69,18 +76,10 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
               <Tasks key={task._id} task={task} />
             ))}
           </div>
-          <div className="sidebar_links">
-            <h2 className="sidebar_subtitle">Links:</h2>
-            {selectedEvent.links.map((link, index) => (
-              <a key={index} href={link} target="_blank" rel="noreferrer">
-                {link}
-              </a>
-            ))}
-          </div>
           <div className="sidebar_budget">
-            <h2 className="sidebar_subtitle">Post Event Check-In</h2>
+            <h2 className="sidebar_subtitle">Logistics</h2>
             <div className="budget_item">
-              <label htmlFor="predictedBudget">Predicted Budget </label>
+              <label htmlFor="predictedBudget">Budget </label>
               <input
                 id="predictedBudget"
                 type="number"
@@ -98,6 +97,13 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
               />
             </div>
             <div className="budget_item">
+              <label htmlFor="attendance">Net </label>
+              <p className='budget_net'
+                style={{ color: net < 0 ? "red" : "green" }}>
+                {net}
+              </p>
+            </div>
+            <div className="budget_item">
               <label htmlFor="attendance">Attendance </label>
               <input
                 id="attendance"
@@ -106,6 +112,14 @@ const Sidebar = ({ selectedEvent, closeSidebar }) => {
                 onChange={(e) => setAttendance(e.target.value)}
               />
             </div>
+          </div>
+          <div className="sidebar_links">
+            <h2 className="sidebar_subtitle">Files</h2>
+            {selectedEvent.links.map((link, index) => (
+              <a key={index} href={link} target="_blank" rel="noreferrer">
+                {link}
+              </a>
+            ))}
           </div>
         </div>
       )}
